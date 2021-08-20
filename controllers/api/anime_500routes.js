983a4637ee1b508973f8dbfe3eec4ba611f059e3
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-08-19 22:41:49 -0700

    Added animelist.handlebars file to test for get request for anime500 titles

--- controllers/api/anime_500routes.js
+++ controllers/api/anime_500routes.js
@@ -3,9 +3,12 @@ const {Anime_500} = require('../../models');
 
 router.get('/', async (req, res) => {
 	try {
-		const animeData = await Anime_500.findAll();
-  
-		res.status(200).json(animeData);
+		const animeData = await Anime_500.findAll({
+			attributes: ['title']
+		});
+		//console.log(animeData);
+		const animes = animeData.map((e) => e.get({ plain: true }));
+		res.render('animelist', {animes});
 	} catch (err) {
 		res.status(400).json(err);
 	}
