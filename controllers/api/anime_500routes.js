3abea7e90c6bc63b04268ba86140bb4dcf8cd893
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-08-24 18:23:10 -0700

    Signup page errors act as desired

--- controllers/api/anime_500routes.js
+++ controllers/api/anime_500routes.js
@@ -4,7 +4,7 @@ const {Anime_500} = require('../../models');
 router.get('/', async (req, res) => {
 	try {
 		const animeData = await Anime_500.findAll({
-			attributes: ['title', 'rating', 'genre', 'image_url'],
+			attributes: ['id', 'title', 'rating', 'genre', 'image_url'],
 		});
 		//console.log(animeData);
 		const animes = animeData.map((e) => e.get({ plain: true }));
--- controllers/api/loggedRoutes.js
+++ controllers/api/loggedRoutes.js
@@ -1,28 +1,41 @@
+/* eslint-disable no-unused-vars */
 const router = require('express').Router();
 //-- add routes to watchlist and user once logged in and authentication path--//
 const { Watch, User } = require('');
 const { Anime_500 } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-        //--should pull anime database--//
-        const animeListing = await Anime_500.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ],
-        });
+router.get('/', withAuth, async (req, res) => {
+	try {
+		//--should pull anime database--//
+		const userWatchlist = await User.findOne({
+			//Find the user with email thats logged in
+			attributes: ['watchlist'],
+			where: { email: req.body.email }
+		});
 
-        const animes = anime_500.map((anime) => anime.get({ plain: true }));
+		const watchlist = userWatchlist.map((anime) => anime.get({ plain: true }));
 
-        res.render('homepage', {
-            animes,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+		res.render('watchlist', {
+			watchlist,
+			logged_in: req.session.logged_in
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
+
+
+//Add anime item to the watchlist
+router.PUT('/user/watchlist/add', async (req, res) => {
+	//get watchlist
+	const anime_id = req.body.anime_id;
+
+});
+
+//Delete anime item from the watchlist
+router.PUT('/user/watchlist/delete', async (req, res) => {
+
+});
+
+module.exports = router;
