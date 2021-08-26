9d6543f027dbf2a43b4f6a9c92b065207d7ea45f
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2021-08-25 22:57:32 -0700

    Once more from the beginning

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 const path = require('path');
 
 const sequelize = require('./config/connection');
@@ -13,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
